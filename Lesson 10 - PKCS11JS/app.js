53d4626b4e7d06728bce6f0b4ae13d5a695013a0
YanuarGuo <yanuar.utama14@gmail.com>
2025-08-27 17:33:17 +0700

    Lesson 10 - HSM and PKCS11 Integration

---
+++ Lesson 10 - PKCS11JS/app.js
@@ -0,0 +1,44 @@
+var pkcs11js = require("pkcs11js");
+var pkcs11 = new pkcs11js.PKCS11();
+
+// pkcs11.load("D:\\SoftHSM2\\lib\\softhsm2-x64.dll");
+// pkcs11.load("D:\\PKCS11 Library\\SafeNet\\cryptoki.dll");
+pkcs11.load("D:\\PKCS11 Library\\OpenSC\\opensc_pkcs11_64.dll");
+
+pkcs11.C_Initialize();
+
+try {
+  // Getting info about PKCS11 Module
+  var module_info = pkcs11.C_GetInfo();
+  console.log(module_info.manufacturerID);
+
+  // Getting list of slots
+  var slots = pkcs11.C_GetSlotList(true);
+  var slot = slots[0];
+
+  // Getting info about slot
+  var slot_info = pkcs11.C_GetSlotInfo(slot);
+  console.log(slot_info.slotDescription);
+
+  // Getting info about token
+  var token_info = pkcs11.C_GetTokenInfo(slot);
+  console.log(token_info.label);
+
+  // Getting info about Mechanism
+  var mechs = pkcs11.C_GetMechanismList(slot);
+  var mech_info = pkcs11.C_GetMechanismInfo(slot, mechs[0]);
+
+  var session = pkcs11.C_OpenSession(
+    slot,
+    pkcs11js.CKF_RW_SESSION | pkcs11js.CKF_SERIAL_SESSION
+  );
+
+  // Getting info about Session
+  var info = pkcs11.C_GetSessionInfo(session);
+  pkcs11.C_Login(session, 1, "648219");
+
+  pkcs11.C_Logout(session);
+  pkcs11.C_CloseSession(session);
+} catch (err) {
+  console.error(err);
+}
